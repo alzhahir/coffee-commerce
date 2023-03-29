379b3af268464b1803f1b8dc4c6cc1632e7c2fbb
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-03-30 04:38:59 +0800

    Add year

--- internal/footer.php
+++ internal/footer.php
@@ -1,7 +1,10 @@
 <?php
-
-//footer content
-
+    //footer code
+    //get date and time
+    date_default_timezone_set("Asia/Kuala_Lumpur");
+    $dateNow = date('Y-m-d');
+    $timeNow = date('H:i:s');
+    $yearNow = date('Y');
 ?>
     <div class="footer bg-dark py-3">
         <b class="text-light">©️ <?php echo $yearNow; ?> Digittend Systems. All Rights Reserved.</b>
