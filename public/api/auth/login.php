e8bf686d32e15cd826bf76b789f671a9c3e94e24
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-04-08 00:16:34 +0800

    Add auth api endpoints

--- public/api/auth/login.php
+++ public/api/auth/login.php
@@ -1,13 +1,7 @@
-<?php
-
-//login api
-
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Ahvelo Coffee - Processing request...</title>
+        <title>AHVELO - api/auth/login</title>
     </head>
     <body>
         <!--h1>Authenticating...</h1-->
@@ -21,16 +15,22 @@
             
             if(!isset($_SESSION["backPage"])){
                 //backPage is not set, defaulting to login.php
-                $backPage = "/login.php";
+                $backPage = "/index.php";
             }
 
+            if(!isset($_SESSION["errorType"])){
+                //errorType is not set, defaulting to error
+                $errorType = "error";
+            }
+
+            $errorType = $_SESSION["errorType"];
             $backPage = $_SESSION["backPage"];
 
             if(isset($_SESSION["uid"])){
                 //user is logged in already
                 $_SESSION["userErrCode"] = "SESSION_EXISTS";
                 $_SESSION["userErrMsg"] = "You are already logged in. Please log out to log in as another user.";
-                header("refresh:0;url=$backPage?error=true");
+                header("refresh:0;url=$backPage?$errorType");
                 die();
             }
 
@@ -45,13 +45,13 @@
                 if(empty(trim($_POST["signInEmail"]))){
                     $_SESSION["userErrCode"] = "INVALID_EMAIL";
                     $_SESSION["userErrMsg"] = "Email is invalid. Please make sure that your email is valid.";
-                    header("refresh:0;url=$backPage?error=true");
+                    header("refresh:0;url=$backPage?$errorType");
                     die();
                 }
                 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                     $_SESSION["userErrCode"] = "INVALID_EMAIL";
                     $_SESSION["userErrMsg"] = "Email is invalid. Please make sure that your email is valid.";
-                    header("refresh:0;url=$backPage?error=true");
+                    header("refresh:0;url=$backPage?$errorType");
                     die();
                 }
 
@@ -68,14 +68,14 @@
                         if($userEmail == 0 || $userEmail == NULL){
                             $_SESSION["userErrCode"] = "WRONG_CREDS";
                             $_SESSION["userErrMsg"] = "Invalid username or password. Please re-enter the credentials";
-                            header("refresh:0;url=$backPage?error=true");
+                            header("refresh:0;url=$backPage?$errorType");
                             die();
                         }//end if
                         //echo "SUCCESS QUERY USERS TABLE FOR EMAIL!<br>";
                     } else {
                         $_SESSION["userErrCode"] = "MYSQL_ERROR";
                         $_SESSION["userErrMsg"] = "MySQL error encountered: ".mysqli_error($conn)." Please contact the administrator if you believe that this should not happen.";
-                        header("refresh:0;url=$backPage?error=true");
+                        header("refresh:0;url=$backPage?$errorType");
                     }
 
                     mysqli_stmt_close($stmt);
@@ -88,14 +88,14 @@
                 } elseif(strlen(trim($_POST["signInPassword"])) < 8){
                     $_SESSION["userErrCode"] = "INVALID_PASSWORD";
                     $_SESSION["userErrMsg"] = "Password is invalid. Password must have at least 8 characters.";
-                    header("refresh:0;url=$backPage?error=true");
+                    header("refresh:0;url=$backPage?$errorType");
                     die();
                 } else{
                     $password = trim($_POST["signInPassword"]);
                 }
 
                 //get usercreds
-                $getUserCredsSQL = "SELECT user_id, user_pass, user_type FROM users WHERE user_email = (?)";
+                $getUserCredsSQL = "SELECT user_id, user_password, user_type FROM users WHERE user_email = (?)";
                 if ($stmt=mysqli_prepare($conn, $getUserCredsSQL)){
                     mysqli_stmt_bind_param($stmt, "s", $user_email);
 
@@ -104,13 +104,13 @@
                     if(mysqli_stmt_execute($stmt)){
                         $usersArray = mysqli_fetch_array(mysqli_stmt_get_result($stmt));
                         $userId = $usersArray["user_id"];
-                        $userPass = $usersArray["user_pass"];
+                        $userPass = $usersArray["user_password"];
                         $userType = $usersArray["user_type"];
                         //echo "SUCCESS QUERY USERS TABLE!\n";
                     } else {
                         $_SESSION["userErrCode"] = "MYSQL_ERROR";
                         $_SESSION["userErrMsg"] = "MySQL error encountered: ".mysqli_error($conn)." Please contact the administrator if you believe that this should not happen.";
-                        header("refresh:0;url=$backPage?error=true");
+                        header("refresh:0;url=$backPage?$errorType");
                         //echo "MYSQL ERROR QUERY USERS TABLE! ".mysqli_error($conn);
 
                     }
@@ -121,55 +121,58 @@
                 if(password_verify($password, $userPass)){
                     //correct password
                     if($userType == 0){
-                        //student
-                        $getStudentInfoSQL = "SELECT student_id, student_name, student_telno, club_id FROM students WHERE user_id = (?)";
-                        if ($stmt=mysqli_prepare($conn, $getStudentInfoSQL)){
+                        //customer
+                        $getCustInfoSQL = "SELECT cust_id, cust_name, cust_dob, cust_gender, student_phone FROM students WHERE user_id = (?)";
+                        if ($stmt=mysqli_prepare($conn, $getCustInfoSQL)){
                             mysqli_stmt_bind_param($stmt, "i", $u_id);
 
                             $u_id = $userId;
 
                             if(mysqli_stmt_execute($stmt)){
-                                $studentArray = mysqli_fetch_array(mysqli_stmt_get_result($stmt));
-                                $studentId = $studentArray["student_id"];
-                                $studentName = $studentArray["student_name"];
-                                $studentTel = $studentArray["student_telno"];
-                                $studentClubId = $studentArray["club_id"];
+                                $custArray = mysqli_fetch_array(mysqli_stmt_get_result($stmt));
+                                $custId = $custArray["cust_id"];
+                                $custName = $custArray["cust_name"];
+                                $custDob = $custArray["cust_dob"];
+                                $custTel = $custArray["cust_phone"];
                                 //echo "SUCCESS QUERY USERS TABLE!<br>";
                             } else {
                                 $_SESSION["userErrCode"] = "MYSQL_ERROR";
                                 $_SESSION["userErrMsg"] = "MySQL error encountered: ".mysqli_error($conn)." Please contact the administrator if you believe that this should not happen.";
-                                header("refresh:0;url=$backPage?error=true");
+                                header("refresh:0;url=$backPage?$errorType");
                                 //echo "MYSQL ERROR QUERY USERS TABLE! ".mysqli_error($conn);
                             }
 
                             mysqli_stmt_close($stmt);
                         }
-                        $_SESSION["utype"] = "student";
+                        $_SESSION["utype"] = "customer";
                         $_SESSION["email"] = $email;
                         $_SESSION["uid"] = $userId;
-                        $_SESSION["name"] = $studentName;
-                        $_SESSION["tel"] = $studentTel;
-                        $_SESSION["student_id"] = $studentId;
-                        $_SESSION["club_id"] = $studentClubId;
-                        header("refresh:0;url=/student/index.php");
-                    } else if ($userType == 1){
-                        //admin
-                        $getAdminInfoSQL = "SELECT admin_id, admin_name, admin_telno FROM admins WHERE user_id = (?)";
-                        if ($stmt=mysqli_prepare($conn, $getAdminInfoSQL)){
+                        $_SESSION["name"] = $custName;
+                        $_SESSION["dob"] = $custDob;
+                        $_SESSION["tel"] = $custTel;
+                        $_SESSION["cust_id"] = $custId;
+                        header("refresh:0;url=/customer/index.php");
+                    } else if ($userType >= 1 && $userType <= 2){
+                        //employees
+                        $getEmpInfoSQL = "SELECT emp_id, emp_name, emp_dob, emp_gender, emp_phone, pos_id FROM employees WHERE user_id = (?)";
+                        if ($stmt=mysqli_prepare($conn, $getEmpInfoSQL)){
                             mysqli_stmt_bind_param($stmt, "i", $u_id);
 
                             $u_id = $userId;
 
                             if(mysqli_stmt_execute($stmt)){
-                                $adminArray = mysqli_fetch_array(mysqli_stmt_get_result($stmt));
-                                $adminId = $adminArray["admin_id"];
-                                $adminName = $adminArray["admin_name"];
-                                $adminTel = $adminArray["admin_telno"];
+                                $empArray = mysqli_fetch_array(mysqli_stmt_get_result($stmt));
+                                $empId = $empArray["emp_id"];
+                                $empName = $empArray["emp_name"];
+                                $empDob = $empArray["emp_dob"];
+                                $empGender = $empArray["emp_gender"];
+                                $empTel = $empArray["emp_phone"];
+                                $posId = $empArray["pos_id"];
                                 //echo "SUCCESS QUERY USERS TABLE!<br>";
                             } else {
                                 $_SESSION["userErrCode"] = "MYSQL_ERROR";
                                 $_SESSION["userErrMsg"] = "MySQL error encountered: ".mysqli_error($conn)." Please contact the administrator if you believe that this should not happen.";
-                                header("refresh:0;url=$backPage?error=true");
+                                header("refresh:0;url=$backPage?$errorType");
                                 //echo "MYSQL ERROR QUERY USERS TABLE! ".mysqli_error($conn);
                             }
 
@@ -178,82 +181,29 @@
                         $_SESSION["utype"] = "admin";
                         $_SESSION["email"] = $email;
                         $_SESSION["uid"] = $userId;
-                        $_SESSION["name"] = $adminName;
-                        $_SESSION["tel"] = $adminTel;
-                        $_SESSION["admin_id"] = $adminId;
-                        header("refresh:0;url=/admin/index.php");
-                    } else if ($userType == 2){
-                        //officer
-                        $getOfficerInfoSQL = "SELECT officer_id, officer_name, officer_telno FROM officers WHERE user_id = (?)";
-                        if ($stmt=mysqli_prepare($conn, $getOfficerInfoSQL)){
-                            mysqli_stmt_bind_param($stmt, "i", $u_id);
-
-                            $u_id = $userId;
-
-                            if(mysqli_stmt_execute($stmt)){
-                                $officerArray = mysqli_fetch_array(mysqli_stmt_get_result($stmt));
-                                $officerId = $officerArray["officer_id"];
-                                $officerName = $officerArray["officer_name"];
-                                $officerTel = $officerArray["officer_telno"];
-                                //echo "SUCCESS QUERY USERS TABLE!<br>";
-                            } else {
-                                $_SESSION["userErrCode"] = "MYSQL_ERROR";
-                                $_SESSION["userErrMsg"] = "MySQL error encountered: ".mysqli_error($conn)." Please contact the administrator if you believe that this should not happen.";
-                                header("refresh:0;url=$backPage?error=true");
-                                //echo "MYSQL ERROR QUERY USERS TABLE! ".mysqli_error($conn);
-                            }
-
-                            mysqli_stmt_close($stmt);
+                        $_SESSION["name"] = $empName;
+                        $_SESSION["tel"] = $empTel;
+                        $_SESSION["admin_id"] = $empId;
+                        $_SESSION["pos_id"] = $posId;
+                        switch($userType){
+                            case 1: //staff
+                                header("refresh:0;url=/staff/index.php");
+                                break;
+                            case 2: //admin
+                                header("refresh:0;url=/admin/index.php");
+                                break;
                         }
-                        $_SESSION["utype"] = "officer";
-                        $_SESSION["email"] = $email;
-                        $_SESSION["uid"] = $userId;
-                        $_SESSION["name"] = $officerName;
-                        $_SESSION["tel"] = $officerTel;
-                        $_SESSION["officer_id"] = $officerId;
-                        header("refresh:0;url=/officer/index.php");
-                    } else if ($userType == 3){
-                        //officer
-                        $getAttendeeInfoSQL = "SELECT attendee_id, attendee_name, attendee_telno, attendee_course FROM attendees WHERE user_id = (?)";
-                        if ($stmt=mysqli_prepare($conn, $getAttendeeInfoSQL)){
-                            mysqli_stmt_bind_param($stmt, "i", $u_id);
-
-                            $u_id = $userId;
-
-                            if(mysqli_stmt_execute($stmt)){
-                                $attendeeArray = mysqli_fetch_array(mysqli_stmt_get_result($stmt));
-                                $attendeeId = $attendeeArray["attendee_id"];
-                                $attendeeName = $attendeeArray["attendee_name"];
-                                $attendeeTel = $attendeeArray["attendee_telno"];
-                                $attendeeCourse = $attendeeArray["attendee_course"];
-                                //echo "SUCCESS QUERY USERS TABLE!<br>";
-                            } else {
-                                $_SESSION["userErrCode"] = "MYSQL_ERROR";
-                                $_SESSION["userErrMsg"] = "MySQL error encountered: ".mysqli_error($conn)." Please contact the administrator if you believe that this should not happen.";
-                                header("refresh:0;url=$backPage?error=true");
-                                //echo "MYSQL ERROR QUERY USERS TABLE! ".mysqli_error($conn);
-                            }
-
-                            mysqli_stmt_close($stmt);
-                        }
-                        $_SESSION["utype"] = "attendee";
-                        $_SESSION["email"] = $email;
-                        $_SESSION["uid"] = $userId;
-                        $_SESSION["name"] = $attendeeName;
-                        $_SESSION["tel"] = $attendeeTel;
-                        $_SESSION["attendee_id"] = $attendeeId;
-                        header("refresh:0;url=/attendee/index.php");
                     } else {
                         $_SESSION["userErrCode"] = "WRONG_CREDS";
                         $_SESSION["userErrMsg"] = "Invalid username or password. Please re-enter the credentials";
-                        header("refresh:0;url=$backPage?error=true");
+                        header("refresh:0;url=$backPage?$errorType");
                         //echo '<script>alert("Wrong password. Returning to login page...")</script>';
                         //header("refresh:0;url=$backPage");
                     }
                 } else {
                     $_SESSION["userErrCode"] = "WRONG_CREDS";
                     $_SESSION["userErrMsg"] = "Invalid username or password. Please re-enter the credentials";
-                    header("refresh:0;url=$backPage?error=true");
+                    header("refresh:0;url=$backPage?$errorType");
                 }
             } else {
                 mysqli_close($conn);
