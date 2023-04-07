0ac2d5c0c3c79fa0b4c0b40e43a59f96edd8490b
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-04-08 00:17:12 +0800

    Update footer

--- internal/footer.php
+++ internal/footer.php
@@ -6,9 +6,9 @@
     $timeNow = date('H:i:s');
     $yearNow = date('Y');
 ?>
-    <footer class="flex-wrap footer mt-auto bg-dark py-3 px-3 sticky-bottom">
-        <div class="row align-items-center">
-            <a class="col h3 fw-black text-light text-decoration-none">AHVELO COFFEE</a>
+    <footer class="flex-wrap align-items-center justify-content-center justify-content-md-between footer mt-auto bg-dark py-2 px-3 mt-5 sticky-bottom">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between">
+            <a class="col h3 mb-0 fw-black text-light text-decoration-none d-flex flex-wrap align-items-center">AHVELO COFFEE</a>
             <b class="col fw-medium text-light text-end me-0">Copyright ©️ <?php echo $yearNow; ?> Megat Al Zhahir Daniel. All Rights Reserved.</b>
         </div>
     </footer>
