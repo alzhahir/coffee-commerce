a2640b846631344ee383c80af3adcb58a7aa5688
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-04-08 02:45:10 +0800

    Remove sticky

--- internal/footer.php
+++ internal/footer.php
@@ -6,7 +6,7 @@
     $timeNow = date('H:i:s');
     $yearNow = date('Y');
 ?>
-    <footer class="flex-wrap align-items-center justify-content-center justify-content-md-between footer mt-auto bg-dark py-2 px-3 mt-5 sticky-bottom">
+    <footer class="flex-wrap align-items-center justify-content-center justify-content-md-between footer mt-auto bg-dark py-2 px-3 mt-5">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between">
             <a class="col h3 mb-0 fw-black text-light text-decoration-none d-flex flex-wrap align-items-center">AHVELO COFFEE</a>
             <b class="col fw-medium text-light text-end me-0">Copyright ©️ <?php echo $yearNow; ?> Megat Al Zhahir Daniel. All Rights Reserved.</b>
