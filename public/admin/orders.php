d68474271d8a02f313ec6bb74625fb70aa65c18a
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-02 02:14:55 +0800

    Add admin pages

---
+++ public/admin/orders.php
@@ -0,0 +1,10 @@
+<?php
+include('../../internal/htmlhead.php');
+include('../../internal/adminheader.php');
+?>
+<div class="px-3">
+    <p class="h3 fw-black">ORDERS</p>
+</div>
+<?php
+include('../../internal/footer.php');
+?>
---
+++ public/admin/users.php
@@ -0,0 +1,10 @@
+<?php
+include('../../internal/htmlhead.php');
+include('../../internal/adminheader.php');
+?>
+<div class="px-3">
+    <p class="h3 fw-black">USERS</p>
+</div>
+<?php
+include('../../internal/footer.php');
+?>
