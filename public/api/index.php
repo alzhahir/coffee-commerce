ab5eb7056fc9bd0d40544f37c8345a46f6726eb2
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-02 18:49:46 +0800

    Add index for api endpoint

---
+++ public/api/index.php
@@ -0,0 +1,18 @@
+<?php
+//use this page to store useful variables for global API use
+$SERVERROOT = $_SERVER["DOCUMENT_ROOT"]; //server root, /public/
+$PROJECTROOT = $_SERVER["DOCUMENT_ROOT"] . '/..';
+$isApiPage = true;
+//http_response_code(401);
+//include($SERVERROOT . "/error/401.php");
+include($PROJECTROOT . "/internal/htmlhead.php");
+include($PROJECTROOT . "/internal/apiheader.php");
+?>
+<div class="px-3">
+    <div class="h3 fw-black">
+        AHVELO COFFEE API ENDPOINT
+    </div>
+</div>
+<?php
+include($PROJECTROOT . "/internal/footer.php")
+?>
