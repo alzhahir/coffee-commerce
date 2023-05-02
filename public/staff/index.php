5f2b0b87f837c22452da5cc79b92c922bb1a8d8e
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-02 16:31:05 +0800

    Add access control to staff page

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,33 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION["emp_id"])){
-    $_SESSION["userErrCode"] = "EMP_ID_NOT_SET";
-    $_SESSION["userErrMsg"] = "The session has expired or is invalid. Please login again. Do contact the administrator if you believe that this should not happen.";
-    header("refresh:0;url=/signup.php?error=true");
-    die();
-}
-if (!isset($_SESSION["utype"])){
-    $_SESSION["userErrCode"] = "UTYPE_NOT_SET";
-    $_SESSION["userErrMsg"] = "Invalid user configuration. Please login again. Do contact the administrator if you believe that this should not happen.";
-    header("refresh:0;url=/signup.php?error=true");
-    die();
-}
-if (isset($_SESSION["utype"]) && ($_SESSION["utype"] != "staff" && $_SESSION["utype"] != "admin") ){
-    include('../../internal/htmlhead.php');
-    include('../../internal/header.php');
-?>
-    <div class="px-3 my-auto">
-        <div style="margin:auto; text-align:center; width:50%;">
-            <span class="material-symbols-outlined" style="margin:auto;display:inline-block;font-size:96px;">
-                warning
-            </span>
-            <p class="h3 fw-medium user-select-none">Your account does not have the required privilege to access this page.</p>
-        </div>
-    </div>
-<?php
-    include('../../internal/footer.php');
-    die();
-}
+include('../../internal/staffcontrol.php');
 include('../../internal/htmlhead.php');
 include('../../internal/staffheader.php');
 $shortName = "admin";
