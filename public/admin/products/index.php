9d8772055a6299d5ebe03fde84d492d3ae3a8670
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-03 18:22:03 +0800

    Add frontend to add new products

--- public/admin/products/index.php
+++ public/admin/products/index.php
@@ -6,8 +6,119 @@ include($ROOTPATH . '/internal/htmlhead.php');
 include($ROOTPATH . '/internal/adminheader.php');
 ?>
 <div class="px-3">
-    <p class="h3 fw-black">PRODUCTS</p>
+    <?php 
+        $_SESSION["backPage"] = $_SERVER["PHP_SELF"];
+        //check if $_GET isset
+        if(isset($_GET["error"])){
+            //error exists
+            echo "<div class=\"alert alert-danger\">";
+            if(isset($_SESSION["userErrMsg"])){
+                //get err msg
+                $errMsg = $_SESSION["userErrMsg"];
+                $errCode = $_SESSION["userErrCode"];
+                echo "<h5 class=\"my-0 fw-semibold\" style=\"text-align: justify; text-justify: inter-word;\">$errMsg</h5>";
+                echo "<p class=\"my-0 fst-italic fw-light\">Error code: $errCode</p>";
+            }
+            echo "</div>";
+        }
+        if(isset($_GET["signup"])){
+            echo "<div class=\"alert alert-success\">";
+            if(isset($_SESSION["userErrMsg"])){
+                //get err msg
+                $errMsg = $_SESSION["userErrMsg"];
+                $errCode = $_SESSION["userErrCode"];
+                echo "<h5 class=\"my-0 fw-semibold\" style=\"text-align: justify; text-justify: inter-word;\">$errMsg</h5>";
+            }
+            echo "</div>";
+        }
+    ?>
+    <p class="h3 fw-black">
+        PRODUCTS
+        <button type="button" class="btn btn-primary ahvbutton float-end" data-bs-toggle="modal" data-bs-target="#newProd">
+            + Add Product
+        </button>
+    </p>
 </div>
+<div class="modal fade" id="newProd" tabindex="-1" aria-labelledby="createNewProduct" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title fw-black" id="createNewProduct">CREATE NEW PRODUCT</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="col pb-3">
+                    <p>You can use this form to create new products.</p>
+                    <?php 
+                        $_SESSION["backPage"] = $_SERVER["PHP_SELF"];
+                        //check if $_GET isset
+                        if(isset($_GET["prodstat"])){
+                            if($_GET["prodstat"] == "error"){
+                                //err
+                                echo "<div class=\"alert alert-danger\">";
+                                if(isset($_SESSION["userErrMsg"])){
+                                    //get err msg
+                                    $errMsg = $_SESSION["userErrMsg"];
+                                    $errCode = $_SESSION["userErrCode"];
+                                    echo "<h5 class=\"my-0 fw-semibold\" style=\"text-align: justify; text-justify: inter-word;\">$errMsg</h5>";
+                                    echo "<p class=\"my-0 fst-italic fw-light\">Error code: $errCode</p>";
+                                }
+                                echo "</div>";
+                            } else if($_GET["prodstat"] == "success"){
+                                //noerr
+                                echo "<div class=\"alert alert-success\">";
+                                if(isset($_SESSION["userErrMsg"])){
+                                    //get err msg
+                                    $errMsg = $_SESSION["userErrMsg"];
+                                    $errCode = $_SESSION["userErrCode"];
+                                    echo "<h5 class=\"my-0 fw-semibold\" style=\"text-align: justify; text-justify: inter-word;\">$errMsg</h5>";
+                                }
+                                echo "</div>";
+                            } else {
+                                //echo "Test lol";
+                            }
+                        }
+                    ?>
+                    <form id="newProdForm" action="/api/admin/create/product.php" method="post">
+                        <div class="form-floating mb-3">
+                            <input class="form-control" name="prodName" type="text" placeholder="Product Name" required/>
+                            <label for="name">Product Name</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" name="prodPrice" type="text" placeholder="Product Price" required/>
+                            <label for="price">Unit Price</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" name="prodStock" type="text" placeholder="Stock Amount" required/>
+                            <label for="stock">Stock Amount</label>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary border-0 rounded-pill" data-bs-dismiss="modal">Close</button>
+                <button class="btn btn-primary ahvbutton" form="newProdForm" id="signUpButton" type="submit">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(window).on('load', function(){
+        <?php
+            if(isset($_GET["prodstat"])){
+                //error exists
+                echo "$('#newProd').modal('show');";
+            }
+        ?>
+    })
+    $('#newProd').on('hidden.bs.modal', function(){
+        let url = new URL(window.location.href);
+        url.searchParams.delete('prodstat');
+        //url.searchParams.delete('');
+        window.history.pushState({}, document.title, url);
+        document.location.reload();
+    })
+</script>
 <?php
 include($ROOTPATH . '/internal/footer.php');
 ?>
