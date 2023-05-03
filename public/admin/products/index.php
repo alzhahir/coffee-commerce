b3a94aa986f466a59fa7c2af3e27064441d5356b
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-03 13:32:29 +0800

    Move products to subfolder

---
+++ public/admin/products/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$ROOTPATH = $_SERVER["DOCUMENT_ROOT"] . '/..';
+include($ROOTPATH . '/internal/admincontrol.php');
+include($ROOTPATH . '/internal/htmlhead.php');
+include($ROOTPATH . '/internal/adminheader.php');
+?>
+<div class="px-3">
+    <p class="h3 fw-black">PRODUCTS</p>
+</div>
+<?php
+include($ROOTPATH . '/internal/footer.php');
+?>
