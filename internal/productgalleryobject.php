0e45048e55c76d74b6bacb8aa7a085b77388a87c
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-04 17:48:44 +0800

    Use external file for product gallery

---
+++ internal/productgalleryobject.php
@@ -0,0 +1,45 @@
+<?php
+    $SERVERROOT = $_SERVER["DOCUMENT_ROOT"];
+    $PROJECTROOT = $_SERVER["DOCUMENT_ROOT"] . '/..';
+    ob_start();
+    $included = true;
+    include $SERVERROOT . "/api/get/products.php";
+    $included = false;
+    $prodData = $outputProdArr;
+    $iteration = 0;
+    foreach($prodData["id"] as $currprod){
+        ?>
+        <div class="col w-100 align-items-center justify-content-center align-middle text-center" style="display:inline-block;width:224px!important;">
+            <svg class="flex-row bd-placeholder-img bd-placeholder-img-lg rounded-5" width="200" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect>
+                <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
+            </svg>
+            <div id="prodNameLabel" title="<?php echo($prodData["name"][$iteration])?>" class="user-select-none flex-row fw-bold fs-4 pt-2" style="text-overflow:ellipsis;overflow:hidden;white-space:nowrap;"><?php echo($prodData["name"][$iteration])?></div>
+            <div id="prodPriceLabel" class="user-select-none flex-row fw-normal fs-4 pb-2"><?php echo("RM" . $prodData["price"][$iteration])?></div>
+        <?php
+            if($prodData["stock"][$iteration] > 0){
+        ?>
+            <button class="btn btn-primary ahvbutton flex-row fw-normal rounded-pill fs-4 align-middle text-center border-0 px-4 py-2">
+                <span class="material-symbols-outlined align-middle text-center px-0">
+                    add_shopping_cart
+                </span>
+                Add to Cart
+            </button>
+        <?php
+            } else {
+        ?>
+            <button class="btn btn-primary ahvbutton flex-row fw-normal rounded-pill fs-4 align-middle text-center border-0 px-4 py-2" disabled>
+                <span class="material-symbols-outlined align-middle text-center px-0">
+                    add_shopping_cart
+                </span>
+                Out of Stock
+            </button>
+        <?php
+            }
+        ?>
+        </div>
+    <?php
+    $iteration++;
+    }
+?>
