cba475cbcf7d21ee3b679a0412fcccee1125b404
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-05 02:23:18 +0800

    Change api json data structure

--- public/api/get/products.php
+++ public/api/get/products.php
@@ -6,28 +6,22 @@
         $getProdSQL = "SELECT prod_id, prod_name, prod_img_url, prod_price, prod_stock FROM products";
         $prodRes = mysqli_query($conn, $getProdSQL);
         if(!is_bool($prodRes)){
-            $outputProdId = array();
-            $outputProdName = array();
-            $outputProdImg = array();
-            $outputProdPrice = array();
-            $outputProdStock = array();
             $outputProdArr = array();
             $prodArr = mysqli_fetch_all($prodRes);
             $prodArr = array_values($prodArr);
             foreach($prodArr as $currProd){
-                array_push($outputProdId, $currProd[0]);
-                array_push($outputProdName, $currProd[1]);
-                array_push($outputProdImg, $currProd[2]);
-                array_push($outputProdPrice, $currProd[3]);
-                array_push($outputProdStock, $currProd[4]);
+                $imgUrl = $currProd[2];
+                if($currProd[2] == null){
+                    $imgUrl = null;
+                }
+                array_push($outputProdArr, array_values(array(
+                    "id" => $currProd[0],
+                    "name" => $currProd[1],
+                    "imageURL" => $imgUrl,
+                    "price" => $currProd[3],
+                    "stock" => $currProd[4],
+                )));
             }
-            $outputProdArr = array(
-                "id" => $outputProdId,
-                "name" => $outputProdName,
-                "imageURL" => $outputProdImg,
-                "price" => $outputProdPrice,
-                "stock" => $outputProdStock,
-            );
         } else {
             $prodArr = array("0" => "Error");
             header('X-PHP-Response-Code: 500', true, 500);
@@ -36,7 +30,7 @@
         
         if(!isset($included) || !$included){
             header("Content-Type: application/json;");
-            echo json_encode($outputProdArr, JSON_PRETTY_PRINT);
+            echo json_encode(array("data" => $outputProdArr), JSON_PRETTY_PRINT);
             die();
         }
     }
