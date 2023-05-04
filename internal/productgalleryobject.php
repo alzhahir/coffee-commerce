7f75fd1ee0bba350ca5641bc5bb95f1a106e853a
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-05 02:24:20 +0800

    Update frontend

--- internal/productgalleryobject.php
+++ internal/productgalleryobject.php
@@ -7,7 +7,7 @@
     $included = false;
     $prodData = $outputProdArr;
     $iteration = 0;
-    foreach($prodData["id"] as $currprod){
+    foreach($prodData as $currProd){
         ?>
         <div class="col w-100 align-items-center justify-content-center align-middle text-center" style="display:inline-block;width:224px!important;">
             <svg class="flex-row bd-placeholder-img bd-placeholder-img-lg rounded-5" width="200" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false">
@@ -15,12 +15,12 @@
                 <rect width="100%" height="100%" fill="#777"></rect>
                 <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
             </svg>
-            <div id="prodNameLabel" title="<?php echo($prodData["name"][$iteration])?>" class="user-select-none flex-row fw-bold fs-4 pt-2" style="text-overflow:ellipsis;overflow:hidden;white-space:nowrap;"><?php echo($prodData["name"][$iteration])?></div>
-            <div id="prodPriceLabel" class="user-select-none flex-row fw-normal fs-4 pb-2"><?php echo("RM" . $prodData["price"][$iteration])?></div>
+            <div id="prodNameLabel" title="<?php echo($currProd["name"])?>" class="user-select-none flex-row fw-bold fs-4 pt-2" style="text-overflow:ellipsis;overflow:hidden;white-space:nowrap;"><?php echo($currProd["name"])?></div>
+            <div id="prodPriceLabel" class="user-select-none flex-row fw-normal fs-4 pb-2"><?php echo("RM" . $currProd["price"])?></div>
         <?php
-            if($prodData["stock"][$iteration] > 0){
+            if($currProd["stock"] > 0){
         ?>
-            <button class="btn btn-primary ahvbutton flex-row fw-normal rounded-pill fs-4 align-middle text-center border-0 px-4 py-2">
+            <button id="prodShoppingBtn" data-value="<?php echo($currProd["id"]); ?>" class="btn btn-primary ahvbutton flex-row fw-normal rounded-pill fs-4 align-middle text-center border-0 px-4 py-2">
                 <span class="material-symbols-outlined align-middle text-center px-0">
                     add_shopping_cart
                 </span>
@@ -29,7 +29,7 @@
         <?php
             } else {
         ?>
-            <button class="btn btn-primary ahvbutton flex-row fw-normal rounded-pill fs-4 align-middle text-center border-0 px-4 py-2" disabled>
+            <button data-value="0" class="btn btn-primary ahvbutton flex-row fw-normal rounded-pill fs-4 align-middle text-center border-0 px-4 py-2" disabled>
                 <span class="material-symbols-outlined align-middle text-center px-0">
                     add_shopping_cart
                 </span>
