708996c3b78cc5b1987dc8d7abb0641d64c1436f
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-05 17:03:19 +0800

    Move toast script to footer

--- internal/footer.php
+++ internal/footer.php
@@ -6,14 +6,46 @@
     $timeNow = date('H:i:s');
     $yearNow = date('Y');
 ?>
-    <div class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
+<div class="toast-container mb-5 me-3 bottom-0 end-0">
+    <div id="toastErr" class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="d-flex">
             <div class="toast-body">
-                Hello, world! This is a toast message.
+                Failed to add item to Your Cart!
             </div>
             <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
     </div>
+    <div id="toastSucc" class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+            <div class="toast-body">
+                The item has been added to Your Cart.
+            </div>
+            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+    </div>
+</div>
+    <script>
+        $(document).on('click', '#prodShoppingBtn', function(){
+            btnData = $(this).data("value");
+            alert(btnData);
+            $.post("/api/user/post/cart.php",
+            {
+                value: btnData,
+            })
+            .done(function(){
+                //success
+                const toastElList = document.querySelectorAll('#toastSucc')
+                const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, {autohide:false, animation:true, delay:3000}))
+                toastElList.foreach(toast => toast.show());
+            })
+            .fail(function(){
+                //fail
+                const toastElList = document.querySelectorAll('#toastErr')
+                const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, {autohide:false, animation:true, delay:3000}))
+                toastList.forEach(toast => toast.show());
+            });
+        });
+    </script>
     <footer class="flex-wrap align-items-center justify-content-center justify-content-md-between footer mt-auto bg-dark py-2 px-3 mt-5">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between">
             <a class="col h3 mb-0 fw-black text-light text-decoration-none d-flex flex-wrap align-items-center">AHVELO COFFEE</a>
