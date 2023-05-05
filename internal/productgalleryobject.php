00f6537126fb8716f440c2b23ee5e6c5955c3217
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-05 17:02:26 +0800

    Update product gallery object

--- internal/productgalleryobject.php
+++ internal/productgalleryobject.php
@@ -7,6 +7,9 @@
     $included = false;
     $prodData = $outputProdArr;
     $iteration = 0;
+?>
+
+<?php
     foreach($prodData as $currProd){
         ?>
         <div class="col w-100 align-items-center justify-content-center align-middle text-center" style="display:inline-block;width:224px!important;">
@@ -15,12 +18,12 @@
                 <rect width="100%" height="100%" fill="#777"></rect>
                 <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
             </svg>
-            <div id="prodNameLabel" title="<?php echo($currProd["name"])?>" class="user-select-none flex-row fw-bold fs-4 pt-2" style="text-overflow:ellipsis;overflow:hidden;white-space:nowrap;"><?php echo($currProd["name"])?></div>
-            <div id="prodPriceLabel" class="user-select-none flex-row fw-normal fs-4 pb-2"><?php echo("RM" . $currProd["price"])?></div>
+            <div id="prodNameLabel" title="<?php echo($currProd[1])?>" class="user-select-none flex-row fw-bold fs-4 pt-2" style="text-overflow:ellipsis;overflow:hidden;white-space:nowrap;"><?php echo($currProd[1])?></div>
+            <div id="prodPriceLabel" class="user-select-none flex-row fw-normal fs-4 pb-2"><?php echo("RM" . $currProd[3])?></div>
         <?php
-            if($currProd["stock"] > 0){
+            if($currProd[4] > 0){
         ?>
-            <button id="prodShoppingBtn" data-value="<?php echo($currProd["id"]); ?>" class="btn btn-primary ahvbutton flex-row fw-normal rounded-pill fs-4 align-middle text-center border-0 px-4 py-2">
+            <button id="prodShoppingBtn" data-value="<?php echo($currProd[0]); ?>" class="btn btn-primary ahvbutton flex-row fw-normal rounded-pill fs-4 align-middle text-center border-0 px-4 py-2">
                 <span class="material-symbols-outlined align-middle text-center px-0">
                     add_shopping_cart
                 </span>
