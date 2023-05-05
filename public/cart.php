8a2dd2d4f6cb8dbf1007c22e4918907931ac40f8
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-05 16:08:19 +0800

    Update cart frontend

---
+++ public/cart.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include('../internal/htmlhead.php');
+include('../internal/header.php');
+?>
+<div class="px-3 ">
+    <h3 class="fw-black">YOUR CART</h3>
+    <?php include('../internal/notimp.php') ?>
+</div>
+<?php
+include('../internal/footer.php');
+?>
