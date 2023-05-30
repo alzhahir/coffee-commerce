2914a2de522a465d9c2c3731440817875bbac77f
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-05-30 23:26:16 +0800

    Handle empty and nonexistent cart

--- public/api/user/get/cart.php
+++ public/api/user/get/cart.php
@@ -18,69 +18,90 @@
         $cid = $_SESSION["cust_id"];
         $getCartSQL = "SELECT cart_id, cart_date FROM carts WHERE cust_id = $cid";
         $cartRes = mysqli_query($conn, $getCartSQL);
-        if(!is_bool($cartRes)){
-            $outputCartArr = array();
-            $cartArr = mysqli_fetch_all($cartRes);
-            $cartArr = array_values($cartArr);
-            foreach($cartArr as $currCart){
-                $caid = $currCart[0];
-                $getCartItmSQL = "SELECT prod_id, cart_item_qty FROM cart_items WHERE cart_id = $caid";
-                $cartItmRes = mysqli_query($conn, $getCartItmSQL);
-                if(!is_bool($cartItmRes)){
-                    $outputItmArr = array();
-                    $cartItmArr = mysqli_fetch_all($cartItmRes);
-                    $cartItmArr = array_values($cartItmArr);
-                    foreach($cartItmArr as $currItm){
-                        array_push($outputItmArr, array_values(array(
-                            "id" => $currItm[0],
-                            "qty" => $currItm[1],
-                        )));
-                    }
-                } else {
-                    $cartArr = array("0" => "Error");
-                    header('X-PHP-Response-Code: 500', true, 500);
-                    die();
-                }
-                array_push($outputCartArr, array_values(array(
-                    $currCart[0],
-                    $currCart[1],
-                    $outputItmArr,
-                )));
+        $cartNum = mysqli_num_rows($cartRes);
+        $noCart = false;
+        if($cartNum < 1){
+            $noCart = true;
+            if(!isset($included) || !$included){
+                $newOutputArr = array(
+                    "dateCreated" => null,
+                    "data" => [],
+                    "isEmpty" => true,
+                );
+                header("Content-Type: application/json;");
+                echo json_encode($newOutputArr, JSON_PRETTY_PRINT);
+                die();
             }
         } else {
-            $cartArr = array("0" => "Error");
-            header('X-PHP-Response-Code: 500', true, 500);
-            die();
-        }
+            if(!is_bool($cartRes)){
+                $outputCartArr = array();
+                $cartArr = mysqli_fetch_all($cartRes);
+                $cartArr = array_values($cartArr);
+                foreach($cartArr as $currCart){
+                    $caid = $currCart[0];
+                    $getCartItmSQL = "SELECT prod_id, cart_item_qty FROM cart_items WHERE cart_id = $caid";
+                    $cartItmRes = mysqli_query($conn, $getCartItmSQL);
+                    if(!is_bool($cartItmRes)){
+                        $outputItmArr = array();
+                        $cartItmArr = mysqli_fetch_all($cartItmRes);
+                        $cartItmArr = array_values($cartItmArr);
+                        $isEmpty = false;
+                        if(count($cartItmArr) === 0){
+                            $isEmpty = true;
+                        }
+                        foreach($cartItmArr as $currItm){
+                            array_push($outputItmArr, array_values(array(
+                                "id" => $currItm[0],
+                                "qty" => $currItm[1],
+                            )));
+                        }
+                    } else {
+                        $cartArr = array("0" => "Error");
+                        header('X-PHP-Response-Code: 500', true, 500);
+                        die();
+                    }
+                    array_push($outputCartArr, array_values(array(
+                        $currCart[0],
+                        $currCart[1],
+                        $outputItmArr,
+                    )));
+                }
+            } else {
+                $cartArr = array("0" => "Error");
+                header('X-PHP-Response-Code: 500', true, 500);
+                die();
+            }
 
-        if(!isset($included) || !$included){
-            $included = true;
-            include($SERVERROOT . '/api/get/products.php');
-            $included = false;
-            $newItmArr = array();
-            $newOutputArr = array();
-            foreach($outputItmArr as $currItm){
-                foreach($outputProdArr as $currProd){
-                    if($currItm[0] == $currProd[0]){
-                        array_push($newItmArr, array_values(array(
-                            "id" => $currItm[0],
-                            "name" => $currProd[1],
-                            "quantity" => $currItm[1],
-                            "price" => $currProd[3],
-                            "subtotal" => $currProd[3] * $currItm[1]
-                        )));
+            if(!isset($included) || !$included){
+                $included = true;
+                include($SERVERROOT . '/api/get/products.php');
+                $included = false;
+                $newItmArr = array();
+                $newOutputArr = array();
+                foreach($outputItmArr as $currItm){
+                    foreach($outputProdArr as $currProd){
+                        if($currItm[0] == $currProd[0]){
+                            array_push($newItmArr, array_values(array(
+                                "id" => $currItm[0],
+                                "name" => $currProd[1],
+                                "quantity" => $currItm[1],
+                                "price" => $currProd[3],
+                                "subtotal" => $currProd[3] * $currItm[1]
+                            )));
+                        }
                     }
                 }
+                foreach($cartArr as $currCart){
+                    $newOutputArr = array(
+                        "dateCreated" => $currCart[1],
+                        "data" => $newItmArr,
+                        "isEmpty" => $isEmpty,
+                    );
+                }
+                header("Content-Type: application/json;");
+                echo json_encode($newOutputArr, JSON_PRETTY_PRINT);
+                die();
             }
-            foreach($cartArr as $currCart){
-                $newOutputArr = array(
-                    "dateCreated" => $currCart[1],
-                    "data" => $newItmArr,
-                );
-            }
-            header("Content-Type: application/json;");
-            echo json_encode($newOutputArr, JSON_PRETTY_PRINT);
-            die();
         }
     }
     else {
