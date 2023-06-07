81413e2d59c4e99052f6e488a84099f2ab046ba4
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-06-07 22:48:17 +0800

    Implement product image URL

--- internal/productgalleryobject.php
+++ internal/productgalleryobject.php
@@ -11,13 +11,27 @@
 
 <?php
     foreach($prodData as $currProd){
+        switch($currProd[2]){
+            case null:
+                $prodImgUrl = "https://openclipart.org/image/800px/194077";
+                break;
+            case !null:
+                $prodImgUrl = $currProd[2];
+                break;
+            default:
+                $prodImgUrl = "https://openclipart.org/image/800px/194077";
+                break;
+        }
         ?>
         <div class="col w-100 align-items-center justify-content-center align-middle text-center <?php if(str_contains($_SERVER["REQUEST_URI"], "shop.php")){echo "pb-3";} ?>" style="display:inline-block;width:224px!important;">
-            <svg class="flex-row bd-placeholder-img bd-placeholder-img-lg rounded-5" width="200" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <div class="flex-row bd-placeholder-img bd-placeholder-img-lg rounded-5">
+                <img src='<?php echo $prodImgUrl ?>' class="img-fluid rounded-5"></img>
+            </div>
+            <!--svg class="flex-row bd-placeholder-img bd-placeholder-img-lg rounded-5" width="200" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="#777"></rect>
                 <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
-            </svg>
+            </svg-->
             <div id="prodNameLabel" title="<?php echo($currProd[1])?>" class="user-select-none flex-row fw-bold fs-4 pt-2" style="text-overflow:ellipsis;overflow:hidden;white-space:nowrap;"><?php echo($currProd[1])?></div>
             <div id="prodPriceLabel" class="user-select-none flex-row fw-normal fs-4 pb-2"><?php echo("RM" . $currProd[3])?></div>
         <?php
