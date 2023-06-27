778510ac982c0caa259d244e64fb111116608851
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-06-28 03:50:02 +0800

    Redirect admins to staff

--- public/admin/orders/index.php
+++ public/admin/orders/index.php
@@ -8,6 +8,15 @@ include($ROOTPATH . '/internal/adminheader.php');
 <div class="px-3">
     <p class="h3 fw-black">ORDERS</p>
 </div>
+<div class="px-3 my-auto">
+    <div style="margin:auto; text-align:center; width:50%;">
+        <span class="material-symbols-outlined" style="margin:auto;display:inline-block;font-size:96px;">
+            transfer_within_a_station
+        </span>
+        <p class="h3 fw-medium user-select-none">Please switch to the staff dashboard to view orders.</p>
+        <a href="/staff/orders/index.php" class="rounded-pill btn btn-outline-danger">GO TO ORDERS</a>
+    </div>
+</div>
 <?php
 include($ROOTPATH . '/internal/footer.php');
 ?>
