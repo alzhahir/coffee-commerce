97a84b0957af3e8a079012da26e0d9cc4338ad4d
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-07-01 17:14:52 +0800

    Fix notification layout

--- internal/header.php
+++ internal/header.php
@@ -381,7 +381,7 @@
         if($('#notifBadge').is(':hidden')){
             $('#notifBadge').show();
         }
-        $('#notifContent').append("<div id='notif"+payload.data.id+"' class='my-2 border border-1 row mx-auto py-3 rounded-4 position-relative'><input onclick='closeNotif(this.dataset.value)' data-value="+payload.data.id+" type=\"button\" class=\"my-2 mx-2 btn-notif-close position-absolute top-0 end-0 btn-close\" aria-label=\"Close\"></input><div onclick='window.location=\""+payload.data.redirect+"\";'><img width='64px' height='64px' src="+payload.notification.image+" class='col col-auto'></img><div class='col'><span class='row fs-4 fw-bold'>"+payload.notification.title+"</span><span class='row'>"+payload.notification.body+"</span></div></div></div>");
+        $('#notifContent').append("<div id='notif"+payload.data.id+"' class='my-2 border border-1 mx-auto py-3 rounded-4 position-relative'><input onclick='closeNotif(this.dataset.value)' data-value="+payload.data.id+" type=\"button\" class=\"my-2 mx-2 btn-notif-close position-absolute top-0 end-0 btn-close\" aria-label=\"Close\"></input><div class='row me-2 my-2 ms-1' onclick='window.location=\""+payload.data.redirect+"\";'><img width='64px' height='64px' src="+payload.notification.image+" class='col col-auto'></img><div class='col'><span class='row fs-4 fw-bold'>"+payload.notification.title+"</span><span class='row'>"+payload.notification.body+"</span></div></div></div>");
     });
 
     //onload window jquery
@@ -401,7 +401,7 @@
                     console.log('success', res)
                     for (let i = 0; i < res.data.length; i++) {
                         element = res.data[i];
-                        $('#notifContent').append("<div id='notif"+element[0]+"' class='my-2 border border-1 row mx-auto py-3 rounded-4 position-relative'><input onclick='closeNotif(this.dataset.value)' data-value="+element[0]+" type=\"button\" class=\"my-2 mx-2 btn-notif-close position-absolute top-0 end-0 btn-close\" aria-label=\"Close\"></input><div onclick='window.location=\""+element[4]+"\";'><img width='64px' height='64px' src="+element[3]+" class='col col-auto'></img><div class='col'><span class='row fs-4 fw-bold'>"+element[1]+"</span><span class='row'>"+element[2]+"</span></div></div></div>");
+                        $('#notifContent').append("<div id='notif"+element[0]+"' class='my-2 border border-1 mx-auto py-3 rounded-4 position-relative'><input onclick='closeNotif(this.dataset.value)' data-value="+element[0]+" type=\"button\" class=\"my-2 mx-2 btn-notif-close position-absolute top-0 end-0 btn-close\" aria-label=\"Close\"></input><div class='row me-2 my-2 ms-1' onclick='window.location=\""+element[4]+"\";'><img width='64px' height='64px' src="+element[3]+" class='col col-auto'></img><div class='col'><span class='row fs-4 fw-bold'>"+element[1]+"</span><span class='row'>"+element[2]+"</span></div></div></div>");
                     }
                 }
             },
