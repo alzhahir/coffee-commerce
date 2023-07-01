628f93fe1ac2aabfe11d2efd5a9a771394885c53
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-07-01 14:10:58 +0800

    Test firebase sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -22,15 +22,36 @@ firebase.initializeApp(firebaseConfig);
 messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
     console.log(
-      '[firebase-messaging-sw.js] Received background message ',
-      payload
+		'[firebase-messaging-sw.js] Received background message ',
+		payload
     );
     // Customize notification here
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
-      body: payload.notification.body,
-      icon: payload.notification.image
+		body: payload.notification.body,
+		icon: payload.notification.image,
+        tag: payload.data.id,
     };
   
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+self.onnotificationclick = (event) => {
+	console.log("On notification click: ", event.notification.tag);
+	event.notification.close();
+
+	// This looks to see if the current is already open and
+	// focuses if it is
+	event.waitUntil(
+		clients
+        .matchAll({
+            type: "window",
+        })
+        .then((clientList) => {
+            for (const client of clientList) {
+            if (client.url === "/" && "focus" in client) return client.focus();
+            }
+            if (clients.openWindow) return clients.openWindow("/");
+        })
+	);
+};
