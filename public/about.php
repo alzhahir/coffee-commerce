eb5cccae36142c515518ad93631f654956644fb2
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-07-01 23:22:27 +0800

    Update about page

--- public/about.php
+++ public/about.php
@@ -9,7 +9,8 @@ $pageTitle = "Ahvelo Coffee - About";
     <div>
         <span class="fw-black fs-5">AHVELO COFFEE AIMS AT SERVING THE HIGHEST QUALITY COFFEE TO EACH AND EVERY CUSTOMERS.</span>
         <p class="fs-5">
-            Ahvelo Coffee.
+            Ahvelo Coffee is a coffee shop located in Kuala Terengganu, Terengganu. The outlet is relatively small, but we aim at serving the highest quality coffee to every customer. 
+            Each cup produced by our talented baristas will surely captivate your heart, mind, and tongue.
         </p>
     </div>
 </div>
