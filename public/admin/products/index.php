5b72a7a80438caa3b8d86b12b5ee23a64ab10fb1
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2023-08-14 09:37:22 +0800

    Update index.php

--- public/admin/products/index.php
+++ public/admin/products/index.php
@@ -40,7 +40,7 @@ include($ROOTPATH . '/internal/adminheader.php');
         </button>
     </p>
     <div class="w-100 pb-4 mb-2 position-relative">
-        <div class='position-absolute top-50 start-50 translate-middle'>
+        <div class='text-nowrap position-absolute top-50 start-50 translate-middle'>
             <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" name="prodStockOptions" id="allChoice" value="all" checked>
                 <label class="form-check-label" for="allChoice">All</label>
